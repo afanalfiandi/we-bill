f4fb1a70a490a64e0628533c6b3eaeae890e725d
Afan Alfi Andi <alfiandiafan@gmail.com>
2025-05-15 09:12:50 +0700

    add: env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  API_URL: 'https://xxx.xxx.xxx/xxx/xxx',
+  API_KEY: 'xxxxxxxxxxx',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+    API_URL: '',
+    API_KEY: ''
+};
